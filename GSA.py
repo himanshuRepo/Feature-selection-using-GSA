a612563f3e741cc7c3d0ffb32aecec86f855c22a
Himanshu Mittal <himanshu.mittal224@gmail.com>
2018-03-05 22:54:56 +0530

    Add files via upload

---
+++ GSA.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Python code of Feature Selection using Gravitational Search Algorithm (GSA) and Support Vector Machine (SVM)
+
+
+Coded by: Mukesh Saraswat (saraswatmukesh@gmail.com), Himanshu Mittal (emailid: himanshu.mittal224@gmail.com) and Raju Pal (emailid: raju3131.pal@gmail.com)
+The code template used is similar given at link: https://github.com/himanshuRepo/GSA_PythonCode and https://github.com/7ossam81/EvoloPy.
+
+Purpose: Main file of Gravitational Search Algorithm(GSA) 
+            for minimizing of the Objective Function
+
+Code compatible:
+ -- Python: 2.* or 3.*
+"""
+
+import random
+import numpy
+import math
+from solution import solution
+import time
+import massCalculation
+import gConstant
+import gField
+import move
+
+        
+def GSA(objf,lb,ub,dim,PopSize,iters,df):
+    # GSA parameters
+    ElitistCheck =1
+    Rpower = 1 
+     
+    s=solution()
+        
+    """ Initializations """
+    
+    vel=numpy.zeros((PopSize,dim))
+    fit = numpy.zeros(PopSize)
+    M = numpy.zeros(PopSize)
+    gBest=numpy.zeros(dim)
+    gBestScore=float("inf")
+    
+    pos=numpy.random.uniform(0,1,(PopSize,dim)) *(ub-lb)+lb
+    
+    convergence_curve=numpy.zeros(iters)
+    
+    print("GSA is optimizing  \""+objf.__name__+"\"")    
+    
+    timerStart=time.time() 
+    s.startTime=time.strftime("%Y-%m-%d-%H-%M-%S")
+    
+    for l in range(0,iters):
+        for i in range(0,PopSize):
+            l1 = [None] * dim
+            l1=numpy.clip(pos[i,:], lb, ub)
+            pos[i,:]=l1
+
+            #Calculate objective function for each particle
+            fitness=[]
+            fitness=-objf(l1,df)
+            fit[i]=fitness
+    
+                
+            if(gBestScore>fitness):
+                gBestScore=fitness
+                gBest=l1
+        
+        """ Calculating Mass """
+        M = massCalculation.massCalculation(fit,PopSize,M)
+
+        """ Calculating Gravitational Constant """        
+        G = gConstant.gConstant(l,iters)        
+        
+        """ Calculating Gfield """        
+        acc = gField.gField(PopSize,dim,pos,M,l,iters,G,ElitistCheck,Rpower)
+        
+        """ Calculating Position """        
+        pos, vel = move.move(PopSize,dim,pos,vel,acc)
+        
+        convergence_curve[l]=gBestScore
+      
+        if (l%1==0):
+                # Just for display, added the negative sign.
+               print(['At iteration '+ str(l+1)+ ' the best fitness is '+ str(-(gBestScore))]);
+    timerEnd=time.time()  
+    s.endTime=time.strftime("%Y-%m-%d-%H-%M-%S")
+    s.executionTime=timerEnd-timerStart
+    s.convergence=convergence_curve
+    s.gBest=gBest
+    s.Algorithm="GSA"
+    s.objectivefunc=objf.__name__
+
+    return s
+         
+    
---
+++ benchmarks.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Python code of Feature Selection using Gravitational Search Algorithm (GSA) and Support Vector Machine (SVM)
+
+
+Coded by: Mukesh Saraswat (saraswatmukesh@gmail.com), Himanshu Mittal (emailid: himanshu.mittal224@gmail.com) and Raju Pal (emailid: raju3131.pal@gmail.com)
+The code template used is similar given at link: https://github.com/himanshuRepo/GSA_PythonCode and https://github.com/7ossam81/EvoloPy.
+
+ -- Purpose: Define the objective function for feature selection as minimization of accuracy_score (cost function)
+              and parameters: function Name, lowerbound, upperbound, dimensions
+
+Code compatible:
+ -- Python: 2.* or 3.*
+"""
+
+import numpy
+import math
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+def F1(indi,df,x=None):
+  """ Fitness function for selected features """
+  # a user-defined threshold value
+  thr=0.5
+  if x is None:
+    x = []
+  x = list(indi)
+  x=numpy.asarray(x)
+  x=x.reshape(1, -1)
+  for i in range(x.shape[1]):
+    if x[0,i]> thr:
+      x[0,i]=1.
+    else:
+      x[0,i]=0.
+
+  [_,oneInd]=numpy.where(x == 1.)
+
+
+  # Check number of features selected, if no feature is selected then select the first feature by default
+  if oneInd.size:
+  	pass
+  else:
+  	oneInd=numpy.zeros((1, 1),dtype=numpy.int)
+
+  oneInd=oneInd.reshape(1, -1)
+  # df1=pd.read_csv("Iris.csv")
+  df1=pd.DataFrame(df, copy=True)
+  df2=df1.iloc[:,1:]
+  X=df2.iloc[:, oneInd[0,:]]
+  X=X.values
+  y=df2.iloc[:, -1]
+  y_act=y.astype('category').cat.codes
+  y_act=y_act.values
+  X_train, X_test, y_train, y_test = train_test_split(X, y_act,test_size=0.30)
+  clf = SVC()
+  clf.fit(X_train, y_train)
+  y_pred=clf.predict(X_test)
+  s=(accuracy_score(y_test, y_pred))
+  return s
+
+
+
+
